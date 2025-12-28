bb317892f493f219d16d3317b9afc6b37d94cb18
hanamelese <hanamelese95@gmail.com>
2025-12-28 12:04:01 -0800

    setup the backend part for the side effect prediction for drugs

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize FastAPI
+app = FastAPI(title="Drug Side Effect Prediction API")
+
+# Enable CORS (IMPORTANT)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later restrict to frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load trained model
+model = joblib.load("model/side_effect_model.joblib")
+
+# Input schema
+class PatientInput(BaseModel):
+    Age: int
+    Gender: str
+    Condition: str
+    Drug_Name: str
+    Dosage_mg: float
+    Treatment_Duration_days: int
+
+# Prediction endpoint
+@app.post("/predict")
+def predict_side_effect(data: PatientInput):
+    input_df = pd.DataFrame([data.dict()])
+    prediction = model.predict(input_df)[0]
+    return {"predicted_side_effect": prediction}
